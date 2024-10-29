d1a9316644fdbb45afcfd9c7827370282096374d
2302538J <167964353+2302538J@users.noreply.github.com>
2024-10-29 12:13:31 +0800

    Revert "Add recipe"

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 var express = require('express');
 var bodyParser = require("body-parser");
 var app = express();
-
 const PORT = process.env.PORT || 5050;
 var startPage = "index.html";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("./public"));
-
-const { addRecipe } = require('./utils/RecipeUtil.js')
-app.post('/addRecipe', addRecipe);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/public/" + startPage);
