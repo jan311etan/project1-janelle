d9e3a7a6b81f9c22020a90157fa6ff093769c372
2302538J <2302538J@student.tp.edu.sg>
2024-11-05 20:34:44 +0800

    finifh the add recipe feature and also the view recipe

--- index.js
+++ index.js
@@ -2,11 +2,29 @@ var express = require('express');
 var bodyParser = require("body-parser");
 var app = express();
 const PORT = process.env.PORT || 5050;
+const { readFile } = require('fs').promises;
+const fs = require('fs').promises;
 var startPage = "index.html";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("./public"));
+
+const {addRecipe } = require('./utils/RecipeUtils');
+app.post('/addRecipe', addRecipe);
+
+app.get('/viewRecipe', async (req, res) => {
+    try {
+        const data = await fs.readFile('./utils/recipe.json', 'utf8'); // Adjust the path if necessary
+        const recipes = JSON.parse(data);
+        res.json(recipes); // Send the recipes as JSON
+    } catch (error) {
+        console.error('Error reading recipe file:', error);
+        res.status(500).json({ error: 'Failed to read recipe data' });
+    }
+});
+
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/public/" + startPage);
