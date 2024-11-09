e108eabbf25f187d0c832835df933bffc54910cf
melanie-bartolome <bartolomemelanie1010@gmail.com>
2024-11-10 00:34:17 +0800

    Implemented the frontend for update-recipe

--- models/Recipe.js
+++ models/Recipe.js
@@ -1,19 +1,18 @@
-// can add default values if later is not working 
 class Recipe {
-    constructor(name, image, cookingTime, servings, instructions, ingredients){
+    constructor(name = "Untitled Recipe", image = null, cookingTime = 0, servings = 1, instructions = "", ingredients = []) {
         this.name = name;
         this.image = image;
         this.cookingTime = cookingTime;
         this.servings = servings;
         this.instructions = instructions;
-        // detailed details for ingredients list with amount, units and name
+
+        // Ensure ingredients is an array and map over it to provide default values for each ingredient
         this.ingredients = ingredients.map(ingredient => ({
-            amount: ingredient.amount,
-            units: ingredient.units,
-            name: ingredient.name,
-        }))
+            amount: ingredient.amount || 0,
+            units: ingredient.units || "",
+            name: ingredient.name || "Ingredient"
+        }));
     }
 }
 
 module.exports = { Recipe };
-
--- utils/RecipeUtil.js
+++ utils/RecipeUtil.js
@@ -1,68 +1,76 @@
 const { Recipe } = require('../models/Recipe');
 const fs = require('fs').promises;
 
-// opens JSON file, reads it and serves it back as an object
-// also logs the error and rethrows it, so everyone knows what went down
+// Opens JSON file, reads it, and serves it back as an object
 async function readJSON(filename) {
     try {
         const data = await fs.readFile(filename, 'utf8');
         return JSON.parse(data);
-    } catch (err) { console.error(err); throw err; }
-}
-
-async function writeJSON(object, filename) {
-    try {
-        const allObjects = await readJSON(filename);
-        allObjects.push(object);
-        await fs.writeFile(filename, JSON.stringify(allObjects), 'utf8');
-        return allObjects;
-    } catch (err) { console.error(err); throw err; }
-}
-
-// UPDATE RECIPE
-async function updateRecipe (req, res) {
-    try {
-        const id = req.params.id;
-        const name = req.body.name;
-        const image = req.body.image;
-        const cookingTime = req.body.cookingTime
-        const servings = req.body.servings;
-        const instructions = req.body.instructions;
-        
-        const allRecipes = await readJSON('utils/recipes.json');
-        var modified = false;
-        for (var i = 0; i < allRecipes.length; i++) {
-            var curcurrRecipes = allRecipes[i];
-            if (curcurrRecipes.id == id) {
-                allRecipes[i].name = name;
-                allRecipes[i].image = image;
-                allRecipes[i].cookingTime = cookingTime;
-                allRecipes[i].servings = servings;
-                allRecipes[i].instructions = instructions;
-
-                if (ingredients) {
-                    allRecipes[i].ingredients = ingredients.map(ingredient => ({
-                        amount: ingredient.amount,
-                        units: ingredient.units,
-                        name: ingredient.name
-                    }))
-                }
-                modified = true;
-                break; // exit loop once the recipe is updated
-            }
-        }
-        if (modified) {
-            await fs.writeFile('utils/recipes.json', JSON.stringify(allRecipes), 'utf8');
-            return res.status(201).json({ message: 'Recipe modified successfully!' });
-        } else {
-            return res.status(500).json({ message: 'Error occurred, unable to modify!' });
-   
-        }
-    } catch (error) {
-        return res.status(500).json({ message: error.message });
+    } catch (err) {
+        console.error(err);
+        throw err;
     }
 }
 
-module.exports = {
-    readJSON, writeJSON, updateRecipe
+// Writes JSON data back to file
+async function writeJSON(data, filename) {
+    try {
+        await fs.writeFile(filename, JSON.stringify(data), 'utf8');
+        return data;
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
 }
+
+// UPDATE RECIPE
+async function updateRecipe(req, res) {
+    const id = String(req.params.id); // Ensure id is treated as a string
+    console.log("Received ID:", id);
+
+    const allRecipes = await readJSON('./utils/recipes.json');
+    console.log("Loaded recipes:", allRecipes);
+
+    let modified = false;
+
+    for (let i = 0; i < allRecipes.length; i++) {
+        const currentRecipe = allRecipes[i];
+        console.log("Checking recipe ID:", currentRecipe.id);
+
+        // Force comparison to be between strings
+        if (String(currentRecipe.id) === id) {
+            // Update only if fields are provided in req.body
+            const { name, image, cookingTime, servings, instructions, ingredients } = req.body;
+
+            if (name) allRecipes[i].name = name;
+            if (image) allRecipes[i].image = image;
+            if (cookingTime) allRecipes[i].cookingTime = cookingTime;
+            if (servings) allRecipes[i].servings = servings;
+            if (instructions) allRecipes[i].instructions = instructions;
+            if (ingredients) {
+                allRecipes[i].ingredients = ingredients.map(ingredient => ({
+                    amount: ingredient.amount,
+                    units: ingredient.units,
+                    name: ingredient.name
+                }));
+            }
+
+            modified = true;
+            break; // Exit loop once the recipe is updated
+        }
+    }
+
+    if (modified) {
+        await writeJSON(allRecipes, './utils/recipes.json');
+        return res.status(200).json({ message: 'Recipe modified successfully!' });
+    } else {
+        return res.status(404).json({ message: 'Recipe not found, unable to modify!' });
+    }
+}
+
+
+module.exports = {
+    readJSON,
+    writeJSON,
+    updateRecipe
+};
