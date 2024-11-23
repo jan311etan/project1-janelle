25397eb394f17343f0f94a19f669adf45747b12c
Janelle <jan311e.tan@gmail.com>
2024-11-23 23:34:39 +0800

    Splitted backend codes for Create Feedback and Update Feedback

--- utils/FeedbackUtil.js
+++ utils/FeedbackUtil.js
@@ -1,7 +1,8 @@
+const { addFeedback } = require('./CreateFeedbackUtil');
+const { updateFeedback } = require('./UpdateFeedbackUtil');
 const fs = require('fs').promises;
-const { Feedback } = require('../models/feedback');
 
-// Function to read feedback data from a JSON file
+// Shared functions for reading and writing feedback
 async function readFeedback(filename) {
     try {
         const data = await fs.readFile(filename, 'utf8');
@@ -12,48 +13,12 @@ async function readFeedback(filename) {
     }
 }
 
-// Function to write feedback data to a JSON file
 async function writeFeedback(feedbackList, filename) {
     try {
         await fs.writeFile(filename, JSON.stringify(feedbackList, null, 2), 'utf8');
     } catch (err) {
         console.error(err);
         throw err;
-    }
-}
-
-// Function to add feedback only if no existing feedback is found for the email
-async function addFeedback(email, feedbackText, filename) {
-    if (!filename) {
-        throw new Error("Filename is required but was not provided.");
-    }
-
-    const feedbackList = await readFeedback(filename);
-    const existingFeedback = feedbackList.find(fb => fb.email === email);
-    if (existingFeedback) {
-        throw new Error("Feedback for this email already exists.");
-    }
-
-    const newFeedback = new Feedback(email, feedbackText);
-    feedbackList.push(newFeedback);
-    await writeFeedback(feedbackList, filename);
-}
-
-// Function to update feedback if it exists
-async function updateFeedback(email, feedbackText, filename) {
-    if (!filename) {
-        throw new Error("Filename is required but was not provided.");
-    }
-
-    const feedbackList = await readFeedback(filename);
-    const existingFeedbackIndex = feedbackList.findIndex(fb => fb.email === email);
-
-    if (existingFeedbackIndex !== -1) {
-        // Update existing feedback
-        feedbackList[existingFeedbackIndex].feedbackText = feedbackText;
-        await writeFeedback(feedbackList, filename);
-    } else {
-        throw new Error("Email not found in the database. Unable to update feedback.");
     }
 }
 
@@ -64,9 +29,9 @@ async function getFeedbackByEmail(email, filename) {
 }
 
 module.exports = {
-    updateFeedback,  // update feedback
-    getFeedbackByEmail,   // Single export for retrieving feedback
-    addFeedback // create feedback
+    addFeedback,
+    updateFeedback,
+    getFeedbackByEmail,
+    readFeedback, // Export shared functions for other modules
+    writeFeedback,
 };
-
-
