ad04ac7b016bc0c1b20efaa7551b121c41de3fb7
Janelle <jan311e.tan@gmail.com>
2025-01-14 10:24:08 +0800

    Completed the implementation of real-time monitoring

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("./public"));
 
+const statusMonitor = require('express-status-monitor');
+app.use(statusMonitor());
+
 const { getFeedbackByEmail } = require('./utils/FeedbackUtil');
 const { addFeedback } = require('./utils/CreateFeedbackUtil');
 
